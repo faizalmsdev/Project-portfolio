6c4e17a2ad1744d67b5b55d0d548a58201f7a661
Dextrodark <faizalmohamed5302002@gmail.com>
2023-08-04 23:15:40 +0530

    New project added

--- src/data.js
+++ src/data.js
@@ -7,174 +7,180 @@ const data = [
     },
     {
         id:2,
+        title:'Youtube-basic Clone',
+        desc:'This project is a YouTube clone that uses an API to fetch video data. It has two pages: the home page and the explore page. The home page displays a list of recommended videos, while the explore page displays a list of videos based on the user search query. The project is implemented using HTML, CSS, and JavaScript.',
+        link:'https://github.com/faizalmsdev/Youtube-Clone/tree/main'
+    },
+    {
+        id:3,
         title:'CryptoNexis-Webpage',
         desc:'Cryptonexis: A sleek single-page crypto webpage with real-time data, interactive charts, and a user-friendly interface. Stay ahead in the world of digital assets with this immersive platform for tracking and analyzing cryptocurrencies.',
         link:'https://github.com/faizalmsdev/CryptoNexis-Webpage'
     },
     {
-        id:3,
+        id:4,
         title:'NestHive-RealEstate',
         desc:'A responsive real estate website built with HTML, CSS, and JavaScript allows users to explore property listings, view property details, and interact with the site across various devices and screen sizes.',
         link:'https://github.com/faizalmsdev/NestHive-RealEstate-webpage'
     },
     {
-        id:4,
+        id:5,
         title:'ClassyCrate-Ecommerce',
         desc:'ClassyCrate offers a curated selection of premium products, including fashion accessories, home decor, electronics, and more. The website is designed to provide a seamless shopping experience for customers who appreciate the finer things in life.',
         link:'https://github.com/faizalmsdev/ClassyCrate-Eccomerce-Site'
     },  
     {
-        id:5,
+        id:6,
         title:'BlinkIt-Clone',
         desc:'Blinkit Clone: Your one-stop grocery shopping app! Order groceries with ease, enjoy a seamless user experience, and get them delivered right to your doorstep. Shop smarter with Blinkit Clone!',
         link:'https://github.com/faizalmsdev/BlinkIt-Clone'
     },
     {
-        id:6,
+        id:7,
         title:'CRUD-Table',
         desc:'A CRUD-based site is a web application that allows users to perform four main operations on a database: Create, Read, Update, and Delete (CRUD). These operations enable users to interact with the database, manage data, and perform various actions on the site content.',
         link:'https://github.com/faizalmsdev/CRUD-Table-Project'
     }, 
     {
-        id:7,
+        id:8,
         title:'Food Delivery Page',
         desc:'A Simple Food Delivery static webpage allows users to browse food items, view their details, and place an order for delivery. It can included with various components.',
         link:'https://github.com/faizalmsdev/FO-Delivery_App'
     }, 
     {
-        id:8,
+        id:9,
         title:'Recipe-Api',
         desc:'It is a simple recipe api site where you can get an recipe of foods which can be fetch by using the API',
         link:'https://github.com/faizalmsdev/cookpal-recipe'
     }, 
     {
-        id:9,
+        id:10,
         title:'Basic-Amazon',
         desc:'A basic Amazon cart is an essential part of an e-commerce website that allows users to add items they wish to purchase and manage their shopping cart before proceeding to checkout. The cart typically provides functionalities like adding/removing items, updating quantities, calculating the total cost, and displaying the list of items added.',
         link:'https://github.com/faizalmsdev/basicAmazon'
     },
     {
-        id:10,
+        id:11,
         title:'Todo-App',
         desc:'This project is a simple task list application that allows users to add and remove tasks from a list. The application is built using HTML, CSS, and JavaScript.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/1.Todo%20App'
     },
     {
-        id:11,
+        id:12,
         title:'FAQ Component',
         desc:'FAQ Component: A user-friendly webpage featuring frequently asked questions. Built using HTML, CSS, and JS for an interactive and informative experience.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/10.FAQ%20Component'
     },
     {
-        id:12,
+        id:13,
         title:'Customizable Modal',
         desc:'Customizable Modal: A versatile webpage component offering customizable modals. Powered by HTML, CSS, and JS for interactive and flexible user interactions.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/11.Customizable%20Modal'
     },
     {
-        id:13,
+        id:14,
         title:'Counter',
         desc:'Counter Component: A dynamic webpage element to display and customize counters. Developed using HTML, CSS, and JS for engaging user interactions.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/2.Counter'
     },
     {
-        id:14,
+        id:15,
         title:'Calculator',
         desc:'Calculator App: A feature-rich web-based calculator. Created with HTML, CSS, and JS to provide users with a convenient and efficient calculating experience',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/3.Calculator'
     },
     {
-        id:15,
+        id:16,
         title:'Contact Form',
         desc:'Contact Form: A user-friendly web form for seamless communication. Crafted with HTML, CSS, and JS, enabling visitors to reach out effortlessly.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/4.Contact%20form'
     },
     {
-        id:16,
+        id:17,
         title:'Vegan Store [landing Page]',
         desc:'FZ Vegan Store is a simple website designed to showcase a vegan store and its products. It contains several sections, including a header with navigation, a main area with sections for product promotion and information, and a footer with contact information.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/5.Landing%20page'
     },
     {
-        id:17,
+        id:18,
         title:'Toggleable Sidebar',
         desc:'This is a project that showcases a toggleable sidebar using HTML, CSS, and JS. The sidebar is designed to be used in a dashboard, and it includes a logo, name, profession, search box, and navigation links.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/6.Toggleable%20Sidebar'
     },
     {
-        id:18,
+        id:19,
         title:'ToolTip Component',
         desc:'Tooltip Component: A versatile web element for interactive tooltips. Developed with HTML, CSS, and JS to enhance user experience and content visibility.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/7.ToolTip%20Component'
     },
     {
-        id:19,
+        id:20,
         title:'Hamburger Menu',
         desc:'Hamburger Menu: An intuitive navigation menu for modern websites. Created using HTML, CSS, and JS, offering a compact and user-friendly interface.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/8.Hamburger%20Menu'
     },
     {
-        id:20,
+        id:21,
         title:'Image SLide',
         desc:'Image Slider: A captivating web component to showcase images. Crafted with HTML, CSS, and JS, creating an engaging and visually appealing slideshow.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/9.Image%20Slider'
     },
     {
-        id:21,
+        id:22,
         title:'Age Calculator',
         desc:'Age Calculator: An interactive web tool to calculate age. Developed with HTML, CSS, and JS, providing users with a quick and convenient way to determine their age.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/Age-Calculator'
     },
     {
-        id:22,
+        id:23,
         title:'Cryptex',
         desc:'Cryptex: An intriguing web app for encrypted messages. Designed with HTML, CSS, and JS, ensuring secure communication and an engaging user experience.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/Cryptex'
     },
     {
-        id:22,
+        id:24,
         title:'Dice Roll Simulator',
         desc:'Dice Roll Simulator: A fun-filled web app for virtual dice rolling. Created with HTML, CSS, and JS, bringing excitement and randomness to your screen.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/Dice-Roll-Simulator'
     },
     {
-        id:23,
+        id:24,
         title:'Fetch API Project ',
         desc:'Fetch API Project: An interactive web application utilizing Fetch API. Developed with HTML, CSS, and JS, fetching data from APIs for dynamic content.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/FETCH%20API%20PROJECT'
     },
     {
-        id:24,
+        id:25,
         title:'Linked in single page clone',
         desc:'LinkedIn Clone: A single-page replica of LinkedIn. Developed with HTML, CSS, and JS, offering a similar experience for professional networking.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/LinkedIn-Clone'
     },
     {
-        id:25,
+        id:26,
         title:'Pomodoro Timer',
         desc:'Pomodoro Timer: A productivity tool with HTML, CSS, and JS. Efficiently manages work sessions and breaks, boosting focus and time management.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/Pomodoro%20Timer'
     },
     {
-        id:26,
+        id:27,
         title:'Recipe Radar',
         desc:'Recipe Radar: A delightful web app using HTML, CSS, and JS. Harnesses APIs to discover and display delicious recipes, satisfying every palate.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/Recipe-Radar'
     },
     {
-        id:27,
+        id:28,
         title:'Rock Paper Scissors Game',
         desc:'Stone-Paper-Scissors Game: An enjoyable web game using HTML, CSS, and JS. Challenge the computer in this classic hand game for endless fun!',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/Rock-Paper-Game'
     },
     {
-        id:28,
+        id:29,
         title:'Tip-Calculator',
         desc:'Tip Calculator: A practical web tool with HTML, CSS, and JS. Easily calculate tips based on bill amount and service quality for hassle-free dining.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/Tip-Calculator'
     },
     {
-        id:29,
+        id:30,
         title:'Weather App',
         desc:'Weather App: An informative web app powered by HTML, CSS, and JS. Get real-time weather updates and forecasts for any location with ease.',
         link:'https://github.com/faizalmsdev/100-JavaScript-Projects-to-Boost-Your-Development-Skills/tree/main/Weather-App'
